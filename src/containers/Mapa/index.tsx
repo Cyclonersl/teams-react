7af8783196381ae57d9547ea2ae3be02e9b5a105
Adriano Odorizzi <adriano.rsl@gmail.com>
2022-11-29 11:41:43 -0300

    Adiciona funcionalidade de mapa ao projeto React

--- src/containers/Mapa/index.tsx
+++ src/containers/Mapa/index.tsx
@@ -1,5 +1,17 @@
+import { useRef } from 'react';
+
+import { Map } from 'leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+
 function Mapa() {
-    return <div className="border-1 border-black m-2 h-[460px] flex items-center justify-center rounded-md">MAPA</div>
+
+    const map = useRef<Map>(null)
+
+    return (
+        <MapContainer center={[-27.5969, -48.5495]} zoom={12} scrollWheelZoom={true} ref={map}>
+            <TileLayer url="https://maps.casan.com.br/tile/{z}/{x}/{y}.png?auth_token=12345678" />
+        </MapContainer>
+    );
 }
 
 export { Mapa }
